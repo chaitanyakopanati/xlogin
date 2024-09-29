0e8b6c1823839d5ebe8fa066370564341be20e66
DESKTOP-3L921JD\admin <sirisha.kopanati@sportsit.net>
2024-09-29 15:20:10 +0530

    This is the first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (username === "user" && password === "password") {
+      setMessage("Welcome, user!");
+    } else {
+      setMessage("Invalid username or password");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Login Page</h1>
+
+      {message && <div className="message">{message}</div>}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="username"
+          required
+        />
+
+        <br />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="password"
+          required
+        />
+
+        <br />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
